a6e7fb7c68554ed71ca5835215e431e36b461011
Emanuel Castillo <root@emacastillozam.localdomain>
2020-06-20 22:57:02 +0200

    Finishing video no. 7

---
+++ backendNode/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+
+var app = express()
+
+app.use(router)
+
+router.get('/message', (req, res) => {
+  res.send('Lista de mensajes')
+})
+
+router.delete('/message', (req, res) => {
+  res.send('Mensaje añadido')
+})
+// app.use('/', (req, res) => {
+//   res.send('Hola')
+// })
+
+app.listen(3000)
+console.log('La aplicación está escuchando en http://localhost:3000')
