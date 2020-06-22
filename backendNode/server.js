6d8e07dd6e3d13f5c491c8980a2fb4c9a2d87f8b
Emanuel Castillo <ecastillo76@misena.edu.co>
2020-06-22 19:06:46 +0200

    Finishing no. 8

--- backendNode/server.js
+++ backendNode/server.js
@@ -1,16 +1,25 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const router = express.Router()
 
 var app = express()
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(router)
 
 router.get('/message', (req, res) => {
+  console.log(req.headers)
+  res.header({
+    "custom-header": "Nuestro valor personalizado",
+  })
   res.send('Lista de mensajes')
 })
 
-router.delete('/message', (req, res) => {
-  res.send('Mensaje añadido')
+router.post('/message', (req, res) => {
+  console.log(req.query)
+  console.log(req.body)
+  res.status(201).send([{ error: '', body: 'Creado correctamente'}])
 })
 // app.use('/', (req, res) => {
 //   res.send('Hola')
