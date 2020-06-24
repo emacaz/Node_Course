118413fdf503af9c395ec00c7951e3d12ee0b185
Emanuel Castillo <ecastillo76@misena.edu.co>
2020-06-24 17:39:33 +0200

    Update no. 14

--- backendNode/server.js
+++ backendNode/server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+
+const response = require('./network/response')
+
 const router = express.Router()
 
 var app = express()
@@ -13,17 +16,19 @@ router.get('/message', (req, res) => {
   res.header({
     "custom-header": "Nuestro valor personalizado",
   })
-  res.send('Lista de mensajes')
+  response.success(req, res, 'Lista de mensajes')
 })
 
 router.post('/message', (req, res) => {
   console.log(req.query)
-  console.log(req.body)
-  res.status(201).send([{ error: '', body: 'Creado correctamente'}])
+  if (req.query.error == 'ok') {
+    response.error(req, res, 'Error inesperado', 500, 'Es sólo una simulacion de los errores')
+  } else {
+      response.success(req, res, 'Creado correctamente', 201)
+  }
 })
-// app.use('/', (req, res) => {
-//   res.send('Hola')
-// })
+
+app.use('/app', express.static('public'))
 
 app.listen(3000)
 console.log('La aplicación está escuchando en http://localhost:3000')
